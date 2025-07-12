3e7db147860a5396e0382b5b1851a6ca9abf36d6
GameOtaku <113395900+GameOtaku@users.noreply.github.com>
2025-07-12 02:41:12 +0200

    Update LGWinType.java

--- src/main/java/fr/leomelki/loupgarou/classes/LGWinType.java
+++ src/main/java/fr/leomelki/loupgarou/classes/LGWinType.java
@@ -5,17 +5,17 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum LGWinType {
-	VILLAGEOIS("§6§l§oLa partie a été gagnée par le §2§lVillage§6§l§o !"),
-	LOUPGAROU("§6§l§oLa partie a été gagnée par les §c§lLoups-Garous§6§l§o !"),
-	LOUPGAROUBLANC("§6§l§oLa partie a été gagnée par le §c§lLoup-Garou Blanc§6§l§o !"),
-	COUPLE("§6§l§oLa partie a été gagnée par le §d§lcouple§6§l§o !"),
-	ANGE("§6§l§oLa partie a été gagnée par l'§d§lAnge§6§l§o !"),
-	EQUAL("§7§l§oÉgalité§6§l§o, personne n'a gagné la partie !"),
-	SOLO("§6§l§oUn joueur solitaire a gagné la partie!"),//bug si ça s'affiche
-	ASSASSIN("§6§l§oLa partie a été gagnée par l'§1§lAssassin§6§l§o !"),
-	PYROMANE("§6§l§oLa partie a été gagnée par le §6§lPyromane§6§l§o !"),
-	VAMPIRE("§6§l§oLa partie a été gagnée par les §5§lVampires§6§l§o !"),
-	NONE("§4Erreur: §cpersonne n'a gagné la partie.");
+	VILLAGEOIS("§6§l§oThe game was won by §2§lVillage§6§l§o!"),
+	LOUPGAROU("§6§l§oThe game was won by the §c§lWerewolves§6§l§o!"),
+	LOUPGAROUBLANC("§6§l§oThe game was won by the §c§lWhite Werewolf§6§l§o!"),
+	COUPLE("§6§l§oThe game was won by the §d§lCouple§6§l§o!"),
+	ANGE("§6§l§oThe game was won by the §d§lAngel§6§l§o!"),
+	EQUAL("§7§l§oTie§6§l§o, no one won the game!"),
+	SOLO("§6§l§oA lone player won the game!"),//bug si ça s'affiche
+	ASSASSIN("§6§l§oThe game was won by the §1§lAssassin§6§l§o!"),
+	PYROMANE("§6§l§oThe game was won by the §6§lPyromaniac§6§l§o!"),
+	VAMPIRE("§6§l§oThe game was won by the §5§lVampires§6§l§o!"),
+	NONE("§4Error: §no one won the game.");
 	
 	@Getter private final String message;
 }
